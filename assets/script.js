1d80c1673dec363b63e56a1d10873e7e28d4b403
Taylor25et <taylor25et@gmail.com>
2022-02-20 14:48:33 -0800

    added time blocks

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// clock display //
+$('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+ //Save Button//
+$( ".saveBtn" ).on( "click", function() {
+    var value = $(this).siblings('.description').val();
+    var key = $(this).parent().attr('id');
+
+    localStorage.setItem(key, value);
+  });
+
+ // Gets ID //
+$('#6 .description').val(localStorage.getItem('6'))
+
+ // Show current time //
+function currentTime(){
+var currentHour = moment().hour();
+
+ // Loops through every time block //
+$('.time-block').each(function(){
+  var timeBlock = parseInt($(this).attr('id'));
+    if (timeBlock < currentHour) {
+      $(this).addClass('past')
+    } else if (timeBlock === currentHour) {
+      $(this).addClass('present')
+    } else {
+      $(this).addClass('future')
+    }
+  })
+}
+currentTime();
